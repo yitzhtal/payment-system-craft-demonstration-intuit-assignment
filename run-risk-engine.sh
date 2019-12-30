2344338fd8c45e9460ef4cdae47cadf9d86582d4
tyitzhak <tal_yitzhak@intuit.com>
2019-12-31 00:34:23 +0200

    removed skip tests

--- run-risk-engine.sh
+++ run-risk-engine.sh
@@ -1,6 +1,6 @@
 kill -9 $(lsof -t -i:8087)
 echo "Build & Run risk-engine"
 cd risk-engine
-mvn clean install -DskipTests
+mvn clean install
 java -jar target/risk-engine-0.0.1-SNAPSHOT.jar 
 echo "Successfully Build & Run risk-engine"
