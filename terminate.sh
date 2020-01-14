df205738122773b1a6655af9b3cb3c4382d75d8d
Tal Yitzhak <talyitzhak100@gmail.com>
2020-01-14 14:50:26 +0200

    Update terminate.sh

--- terminate.sh
+++ terminate.sh
@@ -2,7 +2,7 @@ echo "Stop & Deleting docker containers"
 docker ps -a
 docker stop rabbit-co
 docker rm rabbit-co
-sudo lsof -i :3360
+sudo lsof -i :3306
 docker stop mysql57
 docker rm mysql57
 pkill -9 -f tomcat
